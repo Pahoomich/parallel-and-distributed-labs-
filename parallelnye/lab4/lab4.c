9da14175079103dd3cf887d89c51e378b8cb3302
DmitryProkin <prokindima@mail.ru>
2019-11-22 01:33:50 +0300

    lab4 commit 4.1

--- parallelnye/lab4/lab4.c
+++ parallelnye/lab4/lab4.c
@@ -1,72 +1,86 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/time.h>
 #include <math.h>
-#include <omp.h>
 
+#ifdef _OPENMP
+    #include <omp.h>
+#else
+    void omp_set_num_threads(int  num) {
+        return​ ;
+    }
+    void omp_set_max_active_levels(int  num) {
+        return​ ;
+    }
+    double omp_get_wtime() {
+        struct  timeval T11;
+        gettimeofday(&T11, NULL);
+        return  (T11.tv_sec) *  1000 + (T11.tv_usec) /  1000;
+    }
 
+#endif
 int main(int argc, char *argv[]) {
     int N;
     int A = 315;
     double T1, T2;
     double percent = 0.;
     long delta_ms;
+    unsigned int i=0;
     N = atoi(argv[1]);
+    double *M1 = malloc(sizeof(double) * N);
+    double *M2 = malloc(sizeof(double) * N / 2);
+    double *M1_pre = malloc(sizeof(double) * N);
+    double *M2_pre = malloc(sizeof(double) * N / 2);
+    double *M2_1 = malloc(sizeof(double) * N / 4);
+    double *M2_2 = malloc(sizeof(double) * N / 4);
 
     T1 = omp_get_wtime();
-
-    for (unsigned int i = 0; i < 50; i++) {
-        /*GENERATE*Заполнить массив исходных данных размером NxN*/
-        double *M1 = malloc(sizeof(double) * N);
-        double *M2 = malloc(sizeof(double) * N / 2);
-        double *M1_pre = malloc(sizeof(double) * N);
-        double *M2_pre = malloc(sizeof(double) * N / 2);
-        double *M2_1 = malloc(sizeof(double) * N / 4);
-        double *M2_2 = malloc(sizeof(double) * N / 4);
-
-
-        printf("i= %d\n", i);
-
-        unsigned int seed = i;
-        for (int k = 0; k < N; k++) {
-
-            M1_pre[k] = rand_r(&seed);
-        }
-        for (int k = 0; k < N / 2; k++) {
-            M2_pre[k] = rand_r(&seed);
-        }
-        omp_set_num_threads(4);
-
-#pragma omp parallel /*for default(none)  shared(M1, N, A)*/
+    omp_set_max_active_levels(2);
+#pragma omp parallel sections
+    {
+#pragma omp section
         {
-
-            if (omp_get_thread_num() == 0) {
-                while (i < 50) {
-                    printf("%f\n", percent);
-                    fflush(stdout);
-                    sleep(1);
+            while  ( 1) {
+                printf( "​ \r​ %f%% \n", percent);
+                fflush(stdout);
+                sleep(1);
+                if (percent ==  100 ) {
+                    printf( "​ \r​ %f%% \n", percent);
+                    break;
                 }
             }
-#pragma omp parallel shared(M1, M1_pre, M2, M2_pre, N, A)
-            {
-#pragma omp for /*shared(M1,M1_pre,seed, N, A)*/
+        }
+#pragma omp section
+        {
+            omp_set_num_threads( 3);
+            for ( i = 0; i < 50; i++) {
+                /*GENERATE*Заполнить массив исходных данных размером NxN*/
+              //  printf("t3_count= %d\n",omp_get_num_threads());
+                //printf("nameThead3= %d\n",omp_get_thread_num());
+              //  printf("i= %d\n",i );
+                    unsigned int seed =i;
+                    percent+=0.5;
+                    for (int k = 0; k < N; k++) {
+                        M1_pre[k] = rand_r(&seed);
+                    }
+                    for (int k = 0; k < N/2; k++) {
+                        M2_pre[k] = rand_r(&seed);
+                    }
+              //  printf("nameThead4= %d\n",omp_get_thread_num());
+               // printf("t4_count= %d\n",omp_get_num_threads());
+#pragma omp parallel for shared (M1_pre,A)
                 for (int k = 0; k < N; k++) {
-                    M1[k] = 1. + M1_pre[k] / (RAND_MAX / (A - 1.));
+                    M1[k] = 1. + M1_pre[k]  / (RAND_MAX / (A - 1.));
                 }
-                percent += 0.1;
-                printf("t1_count= %d\n", omp_get_num_threads());
-                // printf("percent= %f\n", percent );
-#pragma omp for /*shared(M2,M2_pre,seed, N, A)*/
+              //  printf("t4_count= %d\n",omp_get_num_threads());
+#pragma omp parallel for shared(M2_pre, A)
                 for (int j = 0; j < N / 2; j++) {
                     M2[j] = A + M2_pre[j] / (RAND_MAX / (10. * A - A));
 
                 }
-
-
-                /* Решить поставленную задачу, заполнить массив с результатами*/
-//       MAP M1       var 6
-#pragma omp parallel for default(none) shared(M1, N)
+                // }
+#pragma omp parallel for default(none) shared(M1,N)
                 for (int k = 0; k < N; k++) {
                     M1[k] = pow(M1[k] / M_E, 1. / 3.);
                 }
@@ -78,18 +92,15 @@ int main(int argc, char *argv[]) {
                         M2[j] = fabs(cos(M2[j] + M2[j - 1]));
                     }
                     M2[0] = fabs(cos(M2[0]));
+                    percent+=0.5;
                 }
 
-//      M9erge         var 4
+//      Merge         var 4
 #pragma omp parallel for default(none) shared(M1, M2, N)
                 for (int j = 0; j < N / 2; j++) {
                     M2[j] = M1[j] >= M2[j] ? M1[j] : M2[j];
                 }
 
-                /*Отсортировать массив с результатами указанным методом*/
-//      Sort         var 6
-                percent += 0.1;
-                printf("t2 = %d\n", omp_get_thread_num());
                 double key;
 //#pragma omp parallel for default(none) shared(M2, N) private(key, j)
                 int l = 0, j = 0;
@@ -128,11 +139,8 @@ int main(int argc, char *argv[]) {
                         }
                     }
                 }
-
                 l = 0;
                 j = 0;
-                percent += 0.1;
-                printf("t3 = %d\n", omp_get_thread_num());
                 for (int k = 0; k < N / 2; k++) {
                     if (l > N / 4 - 1) {
                         double a = M2_2[j];
@@ -160,37 +168,17 @@ int main(int argc, char *argv[]) {
                         X += sin(M2[k]);
                     }
                 }
+                percent+=1.;
+                printf("%lf\n", X);
+
+
             }
-            continue();
-
         }
-        /*     printf("%s\n","-------------" );
-                for ( int k=0;k<N/4; k++){
-                     printf("M2_1= %f\n  ", M2_1[k]);
-                 }
-             printf("%s\n","-------------" );
-                for ( int k=0;k<N/4; k++){
-                     printf("M2_2= %f\n  ", M2_2[k]);
-                 }
-       printf("t4 = %d\n", omp_get_thread_num());
-        for (int k = 0; k < N / 2; k++) {
-            printf("M2 = %f\n ", M2[k]);
-        }
-//      Reduce
-        double X = 0.;
-        int int_part;
-//#pragma omp parallel for default(none) shared(M2, N) private(int_part) reduction(+:X)
-
-        printf("X = %lf\n", X);*/
-
     }
-
     T2 = omp_get_wtime();
     printf("T1= %f\n", T1);
     printf("T2= %f\n", T2);
     delta_ms = (T2 - T1) * 1000;
     printf("\nN = %d. Milliseconds passed: %1ld\n\n", N, delta_ms);
     return 0;
-}
-
-
+        };
