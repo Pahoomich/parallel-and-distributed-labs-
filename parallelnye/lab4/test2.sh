63a2c0d3951675d32eda92f8f08bc6f5e6f85f28
pahoomich <moshguuy@gmail.com>
2019-11-24 00:51:02 +0300

    added graphics and executable files

---
+++ parallelnye/lab4/test2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo -e "Hello my name is lab4 \v"
+for (( var=700; var <= 31000; var += 3030 ))
+do
+./lab4_without_omp $var
+done
---
+++ parallelnye/lab4/test3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo -e "Hello my name is lab4 \v"
+for (( var=1; var <= 10; var += 1 ))
+do
+./lab4_test_2 31000
+done
